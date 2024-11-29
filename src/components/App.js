d2ee18e17d86b1677cd2479022cb5509fe2e8471
phoebe <23031994@myrp.edu.sg>
2024-11-29 14:53:19 +0800

    test1

--- src/components/App.js
+++ src/components/App.js
@@ -7,21 +7,39 @@ function App() {
 
   const handleAddTask = (newTask) => {
     // TODO: write code to add a new task
+    setTasks((prevTasks) => [
+      ...prevTasks,
+      { id: Date.now(), text: newTask, completed: false },
+    ]);
   };
 
   const handleToggleTask = (id) => {
     // TODO: write code to toggle a task's status
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
   };
 
   const handleDeleteTask = (id) => {
     // TODO: write code to delete a task
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
   };
+
+  
 
   return (
     <div className="app">
       <h1>Task Tracker</h1>
       {/*TODO: add a form to add a new task*/}
       {/*TODO: add a list of tasks*/}
+      <Form onAddTask={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onToggleTask={handleToggleTask}
+        onDeleteTask={handleDeleteTask}
+      />
     </div>
   );
 }
